1c569e6bfd18c2fd8646f42ed1a2d0a5e4e29f6b
Ernesto Colina <ecolina05@gmail.com>
2025-04-20 11:32:20 -0500

    feat: update package configurations and clean up distribution files
    - Update package.json configuration for multiple UI components
    - Remove generated distribution files from button component
    - Enhance component dependencies across accordion, avatar, button, card,
      checkbox, input, radio, select, spinner, switch, tabs, and textarea packages

--- packages/button/dist/button/src/props/sizes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sizes = void 0;
-exports.sizes = {
-    sm: 'px-3 py-2 text-xs',
-    md: 'px-3 py-2 text-sm',
-    lg: 'px-4 py-2 text-base',
-};
--- packages/button/dist/spinner/src/props/sizes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sizes = void 0;
-exports.sizes = {
-    xs: 'w-[16px] h-[16px]',
-    sm: 'w-[20px] h-[20px]',
-    md: 'w-[25px] h-[25px]',
-    lg: 'w-[30px] h-[30px]'
-};
